2f8cbd54f115f3017772c87b323d8e813138e1b7
Smilingny <230502771@qq.com>
2023-03-06 11:25:54 +0800

    Initial commit

---
+++ src/main/java/com/education/dormallocation/DormAllocationApplication.java
@@ -0,0 +1,13 @@
+package com.education.dormallocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormAllocationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DormAllocationApplication.class, args);
+    }
+
+}
