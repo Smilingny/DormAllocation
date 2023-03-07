7bb112cc7d8b486e864fbd632d8d913867be71fb
Smilingny <230502771@qq.com>
2023-03-07 10:43:10 +0800

    添加管理员实体

---
+++ src/main/java/com/education/dormallocation/Entity/Administrator.java
@@ -0,0 +1,48 @@
+package com.education.dormallocation.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Administrator {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private Long admin_id;
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getAdmin_id() {
+        return admin_id;
+    }
+
+    public void setAdmin_id(Long admin_id) {
+        this.admin_id = admin_id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
